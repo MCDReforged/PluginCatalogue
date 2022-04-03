403d4ddc874ba358cc77ba7b03d3f940e9208f4c
Fallen_Breath <yqx791125833@gmail.com>
2022-04-03 16:39:07 +0800

    supports checking specified plugin

--- scripts/manager.py
+++ scripts/manager.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
+from typing import Collection, Optional
 
 from doc_gen import generate_doc
 from plugin_list import get_plugin_list
 
 
-def check():
-	get_plugin_list().fetch_data(meta=True, release=False, fail_hard=True)  # so github api token is not needed
+def check(target_ids: Optional[Collection[str]]):
+	get_plugin_list(target_ids).fetch_data(meta=True, release=False, fail_hard=True)  # so github api token is not needed
 
 
 def update_data():
@@ -18,20 +19,22 @@ def main():
 	parser = ArgumentParser(prog='python manager', description='Plugin Catalogue Manager')
 	subparsers = parser.add_subparsers(title='Command', help='Available commands', dest='subparser_name')
 
-	subparsers.add_parser('check', help='Check the correctness of files in "plugins/"')
+	check_parser = subparsers.add_parser('check', help='Check the correctness of files in "plugins/"')
+	check_parser.add_argument('--id', default='', help='The plugin id to be checked, can be separated by "," just like csv format. By default all plugins will be checked')
+
 	subparsers.add_parser('fetch', help='Fetch metadata and release information from github to "meta/"')
 	subparsers.add_parser('doc', help='Generate user friendly plugin catalogue doc to "catalogue/"')
-	subparsers.add_parser('all', help='Run everything above')
+	subparsers.add_parser('all', help='Run everything above: check, fetch, doc')
 
-	result = parser.parse_args()
-	if result.subparser_name == 'check':
-		check()
-	elif result.subparser_name == 'fetch':
+	args = parser.parse_args()
+	if args.subparser_name == 'check':
+		check(None if args.id == '' else args.id.split(','))
+	elif args.subparser_name == 'fetch':
 		update_data()
-	elif result.subparser_name == 'doc':
+	elif args.subparser_name == 'doc':
 		generate_doc()
-	elif result.subparser_name == 'all':
-		check()
+	elif args.subparser_name == 'all':
+		check(None)
 		update_data()
 		generate_doc()
 	else:
