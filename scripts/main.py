14e651496736d1c57e5980116757c60db3320a37
Fallen_Breath <yqx791125833@gmail.com>
2023-02-24 16:26:25 +0800

    target ids arg name change

--- scripts/main.py
+++ scripts/main.py
@@ -11,36 +11,36 @@ def check(target_ids: Optional[Collection[str]]):
 	get_plugin_list(target_ids).fetch_data(meta=True, release=False, fail_hard=True)  # so github api token is not needed
 
 
-def update_data():
-	plugin_list = get_plugin_list()
+def update_data(target_ids: Optional[Collection[str]]):
+	plugin_list = get_plugin_list(target_ids)
 	plugin_list.fetch_data(fail_hard=False)
 	plugin_list.store_data()
 
 
 def main():
 	parser = ArgumentParser(prog='python manager', description='Plugin Catalogue Manager')
+	parser.add_argument('--targets', default='', help='The target plugin ids to be processed, can be separated by "," just like csv format. By default all plugins will be processed')
+
 	subparsers = parser.add_subparsers(title='Command', help='Available commands', dest='subparser_name')
-
-	check_parser = subparsers.add_parser('check', help='Check the correctness of files in "plugins/"')
-	check_parser.add_argument('--id', default='', help='The plugin id to be checked, can be separated by "," just like csv format. By default all plugins will be checked')
-
+	subparsers.add_parser('check', help='Check the correctness of files in "plugins/"')
 	subparsers.add_parser('fetch', help='Fetch metadata and release information from github to "meta/"')
 	subparsers.add_parser('doc', help='Generate user friendly plugin catalogue doc to "catalogue/"')
 	subparsers.add_parser('all', help='Run everything above: check, fetch, doc')
 
 	args = parser.parse_args()
+	target_ids = None if args.targets == '' else args.targets.split(',')
 	reporter.record_command(args.subparser_name)
 	reporter.record_script_start()
 
 	if args.subparser_name == 'check':
-		check(None if args.id == '' else args.id.split(','))
+		check(target_ids)
 	elif args.subparser_name == 'fetch':
-		update_data()
+		update_data(target_ids)
 	elif args.subparser_name == 'doc':
 		generate_doc()
 	elif args.subparser_name == 'all':
 		check(None)
-		update_data()
+		update_data(target_ids)
 		generate_doc()
 	else:
 		parser.print_help()
