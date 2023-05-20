41a8782451ebb8c3987d2a5ce62dd26eb6c72003
Alex3236 <45303195+alex3236@users.noreply.github.com>
2023-05-20 05:18:19 +0000

    do check

--- scripts/pr_check.py
+++ scripts/pr_check.py
@@ -1,18 +1,30 @@
+'''
+A specified script to check plugins on pull requests.
+'''
+
 import json
 import os
+import sys
 
 from constants import ROOT
 from main import check
 
 with open(os.path.join(ROOT, '.github/outputs/all_changed_and_modified_files.json'), 'r', encoding='utf8') as f:
-    modified_files = json.load(f)
+	modified_files = json.load(f)
 
 folders = []
 
 for f in modified_files:
-    folder_name = os.path.split(os.path.dirname(f))[-1]
-    if folder_name not in folders:
-        folders.append(folder_name)
+	folder_name = os.path.split(os.path.dirname(f))[-1]
+	if folder_name not in folders:
+		folders.append(folder_name)
 
-print(folders)
-# check(folders)
+if not folders:
+	msg = 'Skipped plugin checking as there are no checkable modifications.'
+	print(msg)
+	if 'GITHUB_STEP_SUMMARY' in os.environ:
+		with open(os.environ['GITHUB_STEP_SUMMARY'], 'w') as f:
+			f.write(msg)
+	sys.exit(0)
+
+check(folders)
